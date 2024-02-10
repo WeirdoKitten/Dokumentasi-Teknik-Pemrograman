a510d24e7785e927fae2e7a68d50583e08d2d562
Satria Permata Sejati <satriapermata12@gmail.com>
2024-02-10 14:07:16 +0700

    Add files via upload

---
+++ Java/Soal1.java
@@ -0,0 +1,13 @@
+/*
+ * Program untuk mempelajari tipe data byte
+ */
+
+public class Soal1 {
+    public static void main(String[] args){
+        byte angka1 = 10;
+        byte angka2 = 6;
+        byte hasil =(byte) (angka1 + angka2);
+        
+        System.out.println("Hasil 1 "+hasil);
+    }
+}
---
+++ Java/Soal2.java
@@ -0,0 +1,11 @@
+/*
+ * Program untuk mempelajari cara kerja ternary operator
+*/
+
+public class Soal2 {
+    public static void main(String[] args){
+        int i = 42;
+        String s = (i<40)?"life":(i>50)?"universe":"everything";
+        System.out.println(s);
+    }
+}
